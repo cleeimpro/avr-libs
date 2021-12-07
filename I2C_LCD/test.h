40b38ea253c1a447c2c3ff70582aa6c8369e4348
clee <freudenthaler@improvisation.at>
2021-12-07 08:09:43 +0100

    add test lib

---
+++ I2C_LCD/test.h
@@ -0,0 +1,6 @@
+#ifndef _TEST_H                  // Prevents duplicate
+#define _TEST_H   1              // forward declarations
+
+
+#endif                           // End prevent duplicate forward
+/* _TEST_H */                    // declarations block
